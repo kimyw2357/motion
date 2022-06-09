886122b1166a8221fc299adbca1f588eb426a174
kimyw2357 <bubuchachakk@gmail.com>
2022-06-09 23:11:13 +0900

    TS: NoteComponent

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import { ImageComponent } from './components/page/item/image.js';
+import { NoteComponent } from './components/page/item/note.js';
 import { PageComponent } from './components/page/page.js';
 
 class App {
@@ -9,6 +10,9 @@ class App {
 
     const image = new ImageComponent('imge Title', 'https://picsum.photos/600/300');
     image.attachTo(appRoot, 'beforeend')
+
+    const note = new NoteComponent('Note Title', 'Note Body');
+    note.attachTo(appRoot, 'beforeend');
   }
 
 }
