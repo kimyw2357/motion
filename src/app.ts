24eb9648424963c75a8977a35dba9947356d5231
kimyw2357 <bubuchachakk@gmail.com>
2022-06-12 15:57:38 +0900

    TS: todo

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import { ImageComponent } from './components/page/item/image.js';
 import { NoteComponent } from './components/page/item/note.js';
+import { TodoComponent } from './components/page/item/todo.js';
 import { PageComponent } from './components/page/page.js';
 
 class App {
@@ -13,6 +14,9 @@ class App {
 
     const note = new NoteComponent('Note Title', 'Note Body');
     note.attachTo(appRoot, 'beforeend');
+
+    const todo = new TodoComponent('Todo Title', 'Todo Item');
+    todo.attachTo(appRoot, 'beforeend');
   }
 
 }
