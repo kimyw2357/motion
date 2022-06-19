fa57882bdc6388b44f626b2f9327338e32b2abe2
kimyw2357 <bubuchachakk@gmail.com>
2022-06-19 16:49:42 +0900

    youtubURL

--- src/components/page/item/video.ts
+++ src/components/page/item/video.ts
@@ -2,7 +2,6 @@ import { BaseComponent } from '../../component.js';
 
 export class VideoComponent extends BaseComponent<HTMLElement> {
   constructor(title: string, url: string) {
-    console.log(url);
     super(`<section class="video">
             <div class="video__player">
               <iframe class="video__iframe"></iframe>
@@ -10,9 +9,19 @@ export class VideoComponent extends BaseComponent<HTMLElement> {
             <h3 class="video__title"></h3>
           </section>`);
     const iframe = this.element.querySelector('.video__iframe')! as HTMLIFrameElement;
-    iframe.src = 'https://www.youtube.com/embed/RK2vNnembmw';
+    iframe.src = this.convertToEmbeddedURL(url);
 
     const titleElement = this.element.querySelector('.video__title')! as HTMLHeadingElement;
     titleElement.textContent = title;
+  }
+
+  private convertToEmbeddedURL(url: string): string {
+    const regExp = /^(?:https?:\/\/)?(?:www\.)?(?:(?:youtube.com\/(?:(?:watch\?v=)|(?:embed\/))([a-zA-Z0-9-|_]{11}))|(?:youtu.be\/([a-zA-Z0-9-|_]{11})))/;
+    const match = url.match(regExp);
+    const videoId = match ? match[1] || match[2] : undefined;
+    if (videoId) {
+      return `https://www.youtube.com/embed/${videoId}`;
+    }
+    return url;
   }
 }
